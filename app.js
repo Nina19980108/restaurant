47a3e83809754f4e56fa7849e99f9e556f26b0a6
Nina19980108 <ninagirl8787@gmail.com>
2021-03-22 16:01:24 +0800

    set up main.handlebars

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.render('index')
 })
 
 app.listen(port, () => {
