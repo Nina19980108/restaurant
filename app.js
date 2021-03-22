5418dc2b08b6bda41e17f6207624a0344fec977c
Nina19980108 <ninagirl8787@gmail.com>
2021-03-22 16:17:21 +0800

    finish index.handlebars

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json').results
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurantList: restaurantList })
 })
 
 app.listen(port, () => {
